9ae5a5d0d0ae51455fcf3df68505b06d131f87f2
etnms <etiennenemes@gmail.com>
2025-11-04 08:29:52 -0600

    migrated from cra to vite, added more fonts

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig(() => {
+    return {
+        base: '/IPA-Chart-Generator/',
+        build: {
+            outDir: 'build',
+        },
+        plugins: [react(), VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'IPA Chart Generator',
+                short_name: 'IPA Chart',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#000000',
+                theme_color: '#000000',
+                icons: [
+                    {
+                        src: '/logo.svg',
+                        sizes: '192x192',
+                        type: 'image/svg+xml',
+                    },
+                ],
+            },
+        })],
+    };
+});
